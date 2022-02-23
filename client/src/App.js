3faf343c7e5f736b68e916075221f0bba2a677a1
tomerbabila <tomer.babila@gmail.com>
2022-02-23 22:29:21 +0200

    Change mint max number to 4

--- client/src/App.js
+++ client/src/App.js
@@ -162,8 +162,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 10) {
-      newMintAmount = 10;
+    if (newMintAmount > 4) {
+      newMintAmount = 4;
     }
     setMintAmount(newMintAmount);
   };
